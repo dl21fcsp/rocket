8cdd0574dbf0fe4de108b4eac09d3f73812641e6
dl21fcsp <Dennis@Charlotte.fritz.box>
2017-05-06 19:36:36 +0200

    changing class names

--- assets/script.js
+++ assets/script.js
@@ -26,3 +26,13 @@ var car  = {
 var sayMyName = function(name){
 	alert('My Name is:' + name);
 };		
+
+var doCoolStuff = function(){
+	var currentClassName = document.getElementById('cool').className;
+
+	if(currentClassName == 'cool'){
+	document.getElementById('cool').className = 'cool red';	
+} else {
+	document.getElementById('cool').className = 'cool';
+};
+};
