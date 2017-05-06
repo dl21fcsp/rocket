6429ca189b000cace6c32ba57da0c85185b746f7
dl21fcsp <Dennis@Charlotte.fritz.box>
2017-05-06 19:21:48 +0200

    added some js object and buttons

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+var car  = {
+			make: 'VW',
+			type: 'Polo',
+			color: 'brown',
+			isTurnedOn: false,
+			numberOfWheels: 4,
+			seats: ['seat1', 'seat2', 'seat3', 'seat4', 'seat5'],
+			turnOn: function (){
+				this.isTurnedOn = true;
+				return 'car is turned on';
+			},
+			fly: function (){
+				alert('fly');
+			},
+			switchCar: function(isOn){
+				console.log('turn car ' + isOn);
+				if (isOn == true) {
+					this.isTurnedOn = true;
+				} else {
+					this.isTurnedOn = false;
+				};
+			}
+		};
+		console.log('hello friends');
+
+var sayMyName = function(name){
+	alert('My Name is:' + name);
+};		
